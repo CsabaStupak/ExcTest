e1e66f77cde8f50c2ab55ba3db79051de4926ea8
Csaba Stupak <Csaba.Stupak@gmail.com>
2020-05-23 10:34:39 +0200

    Minimal change to reproduce the exception

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ExcTest.Data;
+using Skclusive.Material.Component;
 
 namespace ExcTest
 {
@@ -29,6 +30,8 @@ namespace ExcTest
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.TryAddMaterialServices(new MaterialConfigBuilder().Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
